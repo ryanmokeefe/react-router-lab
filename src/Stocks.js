dfffc691aa95a804b00b0b9a08edef92b19c5703
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-01-24 07:12:43 -0500

    stocks correctly mapping

--- src/Stocks.js
+++ src/Stocks.js
@@ -1,32 +1,39 @@
-import React, {Component} from 'react'
-import stocks from '../data/stock-data.json'
+import React, {Component} from 'react';
+import stockData from '../data/stock-data.json';
 
 
 class Stocks extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
-            stocks,
+            stocks: [],
             name: '',
             symbol: '',
             open: ''
         }
     }
 
-    render() {
-        // let stocks = this.props.stocks.map((stock, index) => {
-        //     return (
-        //     <div key={index}>
-        //         <img src={stock.img} alt={stock.name} />
-        //         <h3>{stock.name} </h3>
-        //         <p>{stock.open}</p>
+    componentDidMount() {
+            this.setState({
+                stocks: stockData
+            })
+    }
 
-        //     </div>
-        // )})
+    render () {
+
+        let stocks = this.state.stocks.map((stock, index) => {
+            return (
+            <div key={index}>
+                <div> {stock.symbol} </div>
+                <h3>{stock.name} </h3>
+                <p>{stock.open}</p>
+
+            </div>
+        )})
         return (
             <div>
                 <div>stock list</div>
-                {/* {stocks} */}
+                {stocks}
             </div>
         )
     }
